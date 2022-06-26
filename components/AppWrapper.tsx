d28122623cb555a5d7e1560859a109eeba330cbe
Dylan Brown <40218657+dylanbr0wn@users.noreply.github.com>
2022-06-26 11:48:38 -0700

    add error boundary

--- components/AppWrapper.tsx
+++ components/AppWrapper.tsx
@@ -2,11 +2,10 @@ import { Toaster } from "react-hot-toast";
 import Menu from "./Menu";
 
 import * as React from "react";
-import { useTheme } from "next-themes";
 import Welcome from "./Welcome";
+import CustErrorBoundary from "./ErrorBoundary";
 
 const AppWrapper = ({ children }: { children: React.ReactNode }) => {
-    const { resolvedTheme } = useTheme();
     const [mounted, setMounted] = React.useState(false);
     React.useEffect(() => {
         setMounted(true);
@@ -16,16 +15,18 @@ const AppWrapper = ({ children }: { children: React.ReactNode }) => {
         return null;
     }
     return (
-        <div>
-            <div
-                className={`  selection:bg-amber-500 selection:text-amber-200 w-screen text-stone-900 dark:text-amber-100 dark:bg-stone-900 bg-white `}
-            >
-                <Menu />
-                {children}
+        <CustErrorBoundary>
+            <div>
+                <div
+                    className={`  selection:bg-amber-500 selection:text-amber-200 w-screen text-stone-900 dark:text-amber-100 dark:bg-stone-900 bg-white `}
+                >
+                    <Menu />
+                    <CustErrorBoundary>{children}</CustErrorBoundary>
+                </div>
+                <Welcome />
+                <Toaster />
             </div>
-            <Welcome />
-            <Toaster />
-        </div>
+        </CustErrorBoundary>
     );
 };
 
